5b8fa3b6db0ba71bb45cf5efe74e6052b9bc7ed0
dipu0 <dipu0@users.noreply.github.com>
2025-05-19 17:34:26 +0000

    deploy: 399308e02192c104226b3ae8931c22f8e44dcdfc

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3120114191"
+    serviceWorkerVersion: "1115474784"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "canvaskit/chromium/canvaskit.js": "34beda9f39eb7d992d46125ca868dc61",
 "canvaskit/chromium/canvaskit.wasm": "64a386c87532ae52ae041d18a32a3635",
 "canvaskit/chromium/canvaskit.js.symbols": "5a23598a2a8efd18ec3b60de5d28af8f",
-"flutter_bootstrap.js": "05bd5985d14486a1e2ba6cd684054245",
+"flutter_bootstrap.js": "a62c7859fb9e84d2299092e323a4add0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "9a412ed0aec5576146a832f081fe1629",
 "/": "9a412ed0aec5576146a832f081fe1629",
