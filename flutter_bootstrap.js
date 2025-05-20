5659116e3ce2570e4d7ec47378fc2f8993abae74
dipu0 <dipu0@users.noreply.github.com>
2025-05-20 12:36:02 +0000

    deploy: 616d529518a255602d41b50d2095fb424e312501

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1115474784"
+    serviceWorkerVersion: "1656275603"
   }
 });
